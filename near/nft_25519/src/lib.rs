643d82ef4251b4d64722818f0d85e1659ed8dac2
0xkai-vbi <thangld@gfigroup.io>
2024-12-18 12:18:42 +0700

    fix arg token_receiver_id for mint_nft

--- near/nft_25519/src/lib.rs
+++ near/nft_25519/src/lib.rs
@@ -1,27 +1,12 @@
-/*!
-Non-Fungible Token implementation with JSON serialization.
-NOTES:
-  - The maximum balance value is limited by U128 (2**128 - 1).
-  - JSON calls should pass U128 as a base-10 string. E.g. "100".
-  - The contract optimizes the inner trie structure by hashing account IDs. It will prevent some
-    abuse of deep tries. Shouldn't be an issue, once NEAR clients implement full hashing of keys.
-  - The contract tracks the change in storage before and after the call. If the storage increases,
-    the contract requires the caller of the contract to attach enough deposit to the function call
-    to cover the storage cost.
-    This is done to prevent a denial of service attack on the contract by taking all available storage.
-    If the storage decreases, the contract will issue a refund for the cost of the released storage.
-    The unused tokens from the attached deposit are also refunded, so it's safe to
-    attach more deposit than required.
-  - To prevent the deployed contract from being modified or deleted, it should not have any access
-    keys on its account.
-*/
+// NEAR OPENEDU NFT CERTIFICATE CONTRACT
+// NEP 171 STANDARD IMPLEMENTATION
 use near_contract_standards::non_fungible_token::metadata::{
     NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
 };
 use near_contract_standards::non_fungible_token::{Token, TokenId};
 use near_contract_standards::non_fungible_token::NonFungibleToken;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{LazyOption, LookupMap};
+use near_sdk::collections::{LazyOption, LookupMap, UnorderedSet};
 use near_sdk::{
     env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise, log, serde::{Deserialize, Serialize},
 };
@@ -39,6 +24,7 @@ pub struct Contract {
     admin_pub_key: String,
     course_metadata_by_id: LookupMap<CourseId, CourseMetadata>,
     total_balances: u128,
+    all_msg_25519: UnorderedSet<String>,
 }
 
 #[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Clone)]
@@ -53,6 +39,7 @@ pub struct CourseMetadata {
 pub enum CourseStorageKey {
     CourseById,
     AllCourseId,
+    AllMsg25519,
 }
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
@@ -76,8 +63,8 @@ impl Contract {
             owner_id,
             NFTContractMetadata {
                 spec: NFT_METADATA_SPEC.to_string(),
-                name: "Example NEAR non-fungible token".to_string(),
-                symbol: "EXAMPLE".to_string(),
+                name: "OpenEdu Certificates".to_string(),
+                symbol: "OPENEDUCERT".to_string(),
                 icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
                 base_uri: None,
                 reference: None,
@@ -103,6 +90,7 @@ impl Contract {
             admin_pub_key,
             course_metadata_by_id: LookupMap::new(CourseStorageKey::CourseById.try_to_vec().unwrap()),
             total_balances: 0,
+            all_msg_25519: UnorderedSet::new(CourseStorageKey::AllMsg25519.try_to_vec().unwrap())
         }
     }
 
@@ -141,6 +129,12 @@ impl Contract {
             "Invalid signature"
         );
 
+        if self.all_msg_25519.contains(&expected_message) {
+            panic!("UserId with CourseId already minted");
+        } else {
+            self.all_msg_25519.insert(&expected_message);
+        }
+
         self.tokens.internal_mint(token_id, receiver_id, Some(token_metadata))
     }
 
@@ -148,8 +142,8 @@ impl Contract {
     pub fn nft_mint_for_sponsor(
         &mut self,
         token_id: TokenId,
-        token_metadata: TokenMetadata,
         receiver_id: AccountId,
+        token_metadata: TokenMetadata,
         course_id: CourseId
     ) -> Token {
         
@@ -210,7 +204,7 @@ impl Contract {
     pub fn nft_mint(
         &mut self,
         token_id: TokenId,
-        token_owner_id: AccountId,
+        receiver_id: AccountId,
         token_metadata: TokenMetadata,
     ) -> Token {
         assert_eq!(
@@ -219,7 +213,7 @@ impl Contract {
             "Unauthorized"
         );
         self.tokens
-            .internal_mint(token_id, token_owner_id, Some(token_metadata))
+            .internal_mint(token_id, receiver_id, Some(token_metadata))
     }
 
     // Ensure the function is payable to allow NEAR deposits
@@ -291,6 +285,16 @@ impl Contract {
             amount,
             course_id
         );
+    }
+
+    fn change_admin_pubkey(&mut self, new_pubkey: String) {
+        let caller_id = env::predecessor_account_id();
+        let owner_id = self.tokens.owner_id.clone();
+
+        assert_eq!(caller_id, owner_id, "Only the NFT owner can change the admin public key.");
+
+        self.admin_pub_key = new_pubkey.clone();
+        log!("Admin public key changed to {}", new_pubkey);
     }
 
     pub fn get_sponsor_balance(&self, course_id: CourseId, sponsor_id: AccountId) -> Option<u128> {
